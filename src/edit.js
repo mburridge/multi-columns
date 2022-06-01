7eb2636aa2f38fbdcff3a49d0472a9a1bbc505a7
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-06-01 12:03:37 +0200

    Removing unused NumberControlTest
    This component is causing errors in the build, and is not used anywhere else in the project

--- src/edit.js
+++ src/edit.js
@@ -33,8 +33,6 @@ import {
  */
 import "./editor.scss";
 
-import NumberControlTest from "./number-control";
-
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
